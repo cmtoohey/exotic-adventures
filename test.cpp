d9b8877e9dd75f881d04bf674ff73daf9dafb57f
drgurung <drgurung@umich.edu>
2015-02-22 22:42:17 -0500

    devin changes

--- test.cpp
+++ test.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+using std::string;
+using std::cout;
+using std::endl;
+
 
 int main(){
-
-	return 0;
+    string test = "Hello";
+    cout << test << endl;
+    return 0;
 }
