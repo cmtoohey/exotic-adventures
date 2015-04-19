85b84e18f6e902f4a5cc60ffcdc597efd503ee5b
lakesideali <alverge@umich.edu>
2015-04-19 18:03:42 -0400

    Added image of skeleton
    Added the image of the skeleton to the game2 screen so you can tell when
    you are being tracked

--- ControlsBasics-WPF/game2.xaml.cs
+++ ControlsBasics-WPF/game2.xaml.cs
@@ -106,7 +106,7 @@ namespace Microsoft.Samples.Kinect.ControlsBasics
             this.imageSource = new DrawingImage(this.drawingGroup);
 
             // Display the drawing using our image control
-        //    Image.Source = this.imageSource;
+            Image.Source = this.imageSource;
 
             // Look through all sensors and start the first connected one.
             // This requires that a Kinect is connected at the time of app startup.
